4627e6347e044edff60865bd16f5acda90bcabf7
theRingleman <sam.ringleman@gmail.com>
2016-07-20 21:51:12 -0600

    add factory girl methods in rails_helper and modified specs syntax

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,29 +2,29 @@ require 'rails_helper'
 
 describe User do
   it "has a valid factory" do
-    expect(FactoryGirl.build(:user)).to be_valid
+    expect(build(:user)).to be_valid
   end
 
   it "is invalid without an email address" do
-    user = FactoryGirl.build(:user, email: nil)
+    user = build(:user, email: nil)
     expect(user).to be_invalid
   end
 
   it "is invalid without a first name" do
-    user = FactoryGirl.build(:user, first_name: nil)
+    user = build(:user, first_name: nil)
     user.valid?
     expect(user.errors[:first_name]).to include("can't be blank")
   end
 
   it "is invalid without a last name" do
-    user = FactoryGirl.build(:user, last_name: nil)
+    user = build(:user, last_name: nil)
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank")
   end
 
   it "is invalid with a duplicate email address" do
-    FactoryGirl.create(:user, email: "joe@example.com")
-    user = FactoryGirl.build(:user, email: "joe@example.com")
+    create(:user, email: "joe@example.com")
+    user = build(:user, email: "joe@example.com")
     user.valid?
     expect(user.errors[:email]).to include("has already been taken")
   end
@@ -66,7 +66,7 @@ describe User do
 
   describe "#full_name" do
     it "returns a string with the users full name" do
-      user = FactoryGirl.build(:user)
+      user = build(:user)
       expect(user.full_name).to eq "#{user.first_name} #{user.last_name}"
     end
   end
