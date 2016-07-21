5c53315676637d8e155efba9379e209e32a9c527
theRingleman <sam.ringleman@gmail.com>
2016-07-20 18:29:54 -0600

    add test for user#by_letter

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def self.by_letter(letter)
+    where("last_name LIKE ?", "#{letter}%").order(:last_name)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -60,4 +60,68 @@ describe User do
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank")
   end
+
+  it "is invalid with a duplicate email address" do
+    User.create(
+    email: "joe@example.com",
+    password: 'password',
+    first_name: "Joe",
+    last_name: "Smith"
+    )
+    user = User.new(
+    email: "joe@example.com",
+    password: "password",
+    first_name: "Joe",
+    last_name: "Snot"
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+
+  describe "#by_letter" do
+    before :each do
+      @smith = User.create(
+        email: "joe@example.com",
+        password: 'password',
+        first_name: "Joe",
+        last_name: "Smith"
+      )
+      @snape = User.create(
+        email: "snape@example.com",
+        password: 'password',
+        first_name: "Serverous",
+        last_name: "Snape"
+      )
+      @johnson = User.create(
+        email: "joel@example.com",
+        password: 'password',
+        first_name: "Jon",
+        last_name: "Johnson"
+      )
+    end
+
+    context "with matching letters" do
+      it "returns a sorted array by the first letter of the last name" do
+        expect(User.by_letter("S")).to eq [@smith, @snape]
+      end
+    end
+
+    context "with non matching letters" do
+      it "omits results that do not match" do
+        expect(User.by_letter("S")).not_to include @johnson
+      end
+    end
+  end
+
+  describe "#full_name" do
+    it "returns a string with the users full name" do
+      user = User.new(
+        email: "joe@example.com",
+        password: "password",
+        first_name: "Joe",
+        last_name: "Example"
+      )
+      expect(user.full_name).to eq "Joe Example"
+    end
+  end
 end
