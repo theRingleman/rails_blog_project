abc5ef1197e47c7c224359d2958c79bbf88038ee
theRingleman <sam.ringleman@gmail.com>
2016-07-21 10:34:05 -0600

    add posts resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+  has_many :posts
+
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :post do
+    user nil
+  end
+end
