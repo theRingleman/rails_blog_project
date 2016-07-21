b2d9b4cb8418099ba9847f3849232448698612af
theRingleman <sam.ringleman@gmail.com>
2016-07-21 13:44:20 -0600

    add initial post controller outline

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,31 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
