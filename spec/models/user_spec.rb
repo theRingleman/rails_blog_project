1dfe5164e6990f0f485d320e43398b5aafafaf2b
theRingleman <sam.ringleman@gmail.com>
2016-07-20 21:41:21 -0600

    add factory to specs and refactored user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe User do
-  it "has a valid factory set up" do
+  it "has a valid factory" do
     expect(FactoryGirl.build(:user)).to be_valid
   end
 
@@ -10,66 +10,21 @@ describe User do
     expect(user).to be_invalid
   end
 
-  it "is valid with an email address" do
-    user = FactoryGirl.build(:user)
-    expect(user).to be_valid
-  end
-
-  it "is valid with a first name" do
-    user = User.new(
-      email: "joe@example.com",
-      password: "password",
-      first_name: "Joe",
-      last_name: "Example"
-    )
-    expect(user).to be_valid
-  end
-
   it "is invalid without a first name" do
-    user = User.new(
-      email: "joe@example.com",
-      password: "password",
-      first_name: "",
-      last_name: "Example"
-    )
+    user = FactoryGirl.build(:user, first_name: nil)
     user.valid?
     expect(user.errors[:first_name]).to include("can't be blank")
   end
 
-  it "is valid with a last name" do
-    user = User.new(
-      email: "joe@example.com",
-      password: "password",
-      first_name: "Joe",
-      last_name: "Example"
-    )
-    expect(user).to be_valid
-  end
-
   it "is invalid without a last name" do
-    user = User.new(
-      email: "joe@example.com",
-      password: "password",
-      first_name: "Joe",
-      last_name: ""
-    )
+    user = FactoryGirl.build(:user, last_name: nil)
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank")
   end
 
   it "is invalid with a duplicate email address" do
-    User.create(
-    email: "joe@example.com",
-    password: 'password',
-    first_name: "Joe",
-    last_name: "Smith"
-    )
-    user = User.new(
-    email: "joe@example.com",
-    password: "password",
-    first_name: "Joe",
-    last_name: "Snot"
-    )
+    FactoryGirl.create(:user, email: "joe@example.com")
+    user = FactoryGirl.build(:user, email: "joe@example.com")
     user.valid?
     expect(user.errors[:email]).to include("has already been taken")
   end
@@ -111,13 +66,8 @@ describe User do
 
   describe "#full_name" do
     it "returns a string with the users full name" do
-      user = User.new(
-        email: "joe@example.com",
-        password: "password",
-        first_name: "Joe",
-        last_name: "Example"
-      )
-      expect(user.full_name).to eq "Joe Example"
+      user = FactoryGirl.build(:user)
+      expect(user.full_name).to eq "#{user.first_name} #{user.last_name}"
     end
   end
 end
