312af323b4a690de9083328328d9c681571c3c18
theRingleman <sam.ringleman@gmail.com>
2016-07-24 15:11:09 -0600

    Add sort feature for admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
     authorize @users
   end
 
+  def sort
+    @users = User.sort_by(params[:q])
+  end
+
   def show
     @user = User.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: [:index, :new, :create]
   end
-
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   resources :users, only: [:show, :index]
   root 'static#home'
+  get 'sort', to: 'users#sort'
 end
