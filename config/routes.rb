bb51bda8295dea62fee0be522131b97631b8fdf7
theRingleman <sam.ringleman@gmail.com>
2016-08-05 11:11:07 -0600

    add tags show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   end
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   resources :users, only: [:show, :index]
+  resources :tags, only: [:show]
   root 'static#home'
   post 'sort', to: 'users#sort'
 end
