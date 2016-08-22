6c5136120f3cd9380960c446cfd05c196375f6eb
theRingleman <sam.ringleman@gmail.com>
2016-08-22 13:25:34 -0600

    add post and comment serializer as well as the active model serializer gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,9 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    respond_to do |format|
+      format.html {render :index}
+      format.json {render json: @posts}
   end
 
   def show
