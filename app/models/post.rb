0ec4886cd3464b7c47577ba0c011f08074512ca5
theRingleman <sam.ringleman@gmail.com>
2016-08-23 12:08:59 -0600

    finally fixed my multiple comment bug.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,12 +6,16 @@ class Post < ActiveRecord::Base
   has_many :post_tags
   has_many :tags, through: :post_tags
   accepts_nested_attributes_for :tags, reject_if: :all_blank
-  accepts_nested_attributes_for :comments
+  accepts_nested_attributes_for :comments, reject_if: :reject_comments
 
   def tags_attributes=(tag_attributes)
     tag_attributes.values.each do |tag_attribute|
       tag = Tag.find_or_create_by(tag_attribute)
       self.tags << tag
     end
+  end
+
+  def reject_comments(comment)
+    comment[:user_id].blank?
   end
 end
