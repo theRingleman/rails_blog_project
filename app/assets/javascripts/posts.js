322f867c71018e5c7d789662e1f5161ea7648c7f
theRingleman <sam.ringleman@gmail.com>
2016-08-22 21:08:22 -0600

    Long commit... Change comment show html by adding new classes and rendering the html differently, also add experimental js to render the comments through ajax. I also commented out the entire comments controller to see if anything would change and fix my duplicate comment problem. Duplicate problem not yet fixed.

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -4,10 +4,22 @@ function nextPost() {
     var nextId = parseInt($(".js-next").attr("data-id")) + 1;
     $.get("/posts/" + nextId + ".json", function(post) {
       // get post
-      $(".postTitle").text(post["title"]);
-      $(".postContent").text(post["content"]);
+      $(".postTitle").text(post.title);
+      $(".postContent").text(post.content);
       // re-set the id to current on the link
-      $(".js-next").attr("data-id", post["id"]);
+      $(".js-next").attr("data-id", post.id);
+      renderComments(post.comments)
     });
   });
+}
+
+function renderComments(comments) {
+  $('.comment-username').text(comments[0].first_name + " " + comments[0].last_name + " says:");
+  $('.comment-content').text(comments[0].content);
+  for (var i = 1; i < comments.length; i++) {
+    $('.comments').append(
+      $('.comment-username').text(comments[i].first_name + " " + comments[i].last_name + " says:");
+      $('.comment-content').text(comments[i].content);
+    )
+  }
 }
