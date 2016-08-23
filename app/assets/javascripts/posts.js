c7f2ee300d71187bb4b911c82f74960cec55f560
theRingleman <sam.ringleman@gmail.com>
2016-08-22 18:47:03 -0600

    Add preventDefault on click even and add author to json output for post

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,10 +1,11 @@
 function nextPost() {
-  $(".js-next").on("click", function() {
+  $(".js-next").on("click", function(event) {
+    event.preventDefault();
     var nextId = parseInt($(".js-next").attr("data-id")) + 1;
     $.get("/posts/" + nextId + ".json", function(post) {
       // get post
       $(".postTitle").text(post["title"]);
-      $(".postBody").text(post["description"]);
+      $(".postContent").text(post["content"]);
       // re-set the id to current on the link
       $(".js-next").attr("data-id", post["id"]);
     });
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,4 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content
+  attributes :id, :title, :content, :author
   has_many :comments
 end
